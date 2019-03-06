75be1dd7cdcff20fd5a057fc9ac07887a63ba20b
James W. Bradley <jwbradley@users.noreply.github.com>
2019-03-06 10:02:53 -0600

    First Commit
    Uploaded

---
+++ passwordGen.php
@@ -0,0 +1,32 @@
+<?php
+
+function randomPassword($length=10) {
+
+    // variable init
+    $pass          =  '';
+    $verified      =  false;
+    $looper        =  0;
+
+    // Characters Lists
+    $specials      =  '!#$%&()*+,-:;/=>?_';  // Only the specials needed for verifications.
+    $characters    =  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'; // Letters and Numbers
+    $allcharacters =  $characters . $specials;  // Combining strings
+
+    while ( ($verified == false) & $looper < 1000 ){
+        $pass      =  substr(str_shuffle($allcharacters), rand($length, strlen($allcharacters)-$length), $length);
+
+        $verified  =  (((preg_match("#[0-9]+#", $pass)) && (preg_match("#[a-z]+#", $pass)) && (preg_match("#[A-Z]+#", $pass)) && (strpbrk($pass, $specials)) && (strlen(trim($pass)) == $length)) ? true : false);
+
+        echo (!$verified ?"<!-- Password Verify Check Failed for [".$pass."], looping again. -->\n" : '');
+
+        $looper++;  // Keeps the checker process from looping forever. Shouldn't need this many attempts to get a valid password.
+    }
+    return $pass; // return the generated & verified password
+}
+
+$my_passwords = randomPassword(12);
+
+echo "<br /><br />\n\nUse the following randomly generated ".strlen(trim($my_passwords))." character password to signon to the Oracle Instance: \n<pre><code>".$my_passwords."</code></pre><br />\n";
+
+
+?>
